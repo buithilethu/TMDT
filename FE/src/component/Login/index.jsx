4789466261749c568f3b50d837b3688a8fefdd96
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2025-04-17 13:06:52 +0700

    Update index.jsx

--- FE/src/component/Login/index.jsx
+++ FE/src/component/Login/index.jsx
@@ -51,7 +51,7 @@ const Login = () => {
 
       // Lưu thông tin user vào cookie
       Cookies.set('user', JSON.stringify(userData), { expires: 7, secure: true, sameSite: 'Strict' });
-      Cookies.set('token', loginData.accessToken, { expires: 7, secure: true, sameSite: 'none' });
+      Cookies.set('token', loginData.accessToken, { expires: 7, secure: true, sameSite: 'none', domain: 'http://localhost:300' });
 
 
       // Chuyển hướng sang trang chủ
